141d82422a5034a3cbdfd66ad254b21bb3ed22d8
Lukáš Gurecký <l.gurecky@seznam.cz>
2020-12-17 22:26:05 +0100

    feat: add paperclip icon to file input

--- components/FormUploadFile.vue
+++ components/FormUploadFile.vue
@@ -7,7 +7,9 @@ export default {
   },
   methods: {
     previewFiles (event) {
-      this.fileInputText = event.target.files[0].name
+      if (event.target.files[0]) {
+        this.fileInputText = event.target.files[0].name
+      }
     }
   }
 }
@@ -15,7 +17,7 @@ export default {
 
 <template>
   <label class="file-input">
-    <i class="fa fa-image" />
+    <i class="icon--paperclip" />
     {{ fileInputText }}
     <input accept="image/*," type="file" style="display: none;" name="image" @change="previewFiles">
   </label>
@@ -37,5 +39,17 @@ export default {
   width: 100%;
   text-align: center;
   font-weight: bold;
+}
+
+.icon--paperclip {
+  position: relative;
+  top: 3px;
+  left: -3px;
+  width: 1.3em;
+  height: 1.3em;
+  display: inline-block;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-image: url('/paperclip.svg');
 }
 </style>
