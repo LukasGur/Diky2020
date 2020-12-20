1f7599f4f17e1b14d00dbd3d83b44e83276771b7
Lukáš Gurecký <l.gurecky@seznam.cz>
2020-12-20 21:03:57 +0100

    refactor: remove useless orders from nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,12 +84,5 @@ export default {
   },
 
   generate: {
-    subFolders: false,
-    async routes () {
-      const { $content } = require('@nuxt/content')
-      const files = await $content({ deep: true }).only(['path']).fetch()
-
-      return files.map(file => file.path === '/index' ? '/' : file.path)
-    }
   }
 }
