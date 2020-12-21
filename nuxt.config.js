eb46910825e143f551bbecb7850c88217a82030d
Lukáš Gurecký <l.gurecky@seznam.cz>
2020-12-21 16:14:40 +0100

    chore: google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,26 +37,19 @@ export default {
     ]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
-
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     '@nuxtjs/style-resources',
-    '@nuxtjs/axios',
     '@nuxt/content',
     '@nuxtjs/recaptcha',
     'vue-social-sharing/nuxt',
@@ -75,13 +68,6 @@ export default {
     scss: '~/assets/scss/*.scss'
   },
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
-
-  content: {
-    // Options
-  },
-
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     postcss: {
@@ -94,6 +80,8 @@ export default {
     }
   },
 
-  generate: {
+  googleAnalytics: {
+    id: 'G-3ZKQ1FP7VX',
+    checkDuplicatedScript: true
   }
 }
