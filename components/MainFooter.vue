3526cd4c8aabee967eb20063b95ab9adbfbf0633
Lukáš Gurecký <l.gurecky@seznam.cz>
2020-12-21 22:13:14 +0100

    feat: create footer

---
+++ components/MainFooter.vue
@@ -0,0 +1,116 @@
+<script>
+export default {
+  data () {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container footer__wrapper">
+      <a href="mailto:info@diky2020.cz" class="mail-link">
+        <i class="icon icon--email" />info<span class="copyright__icon">@</span>diky2020.cz
+      </a>
+      <div class="copyright">
+        <span class="copyright__icon">&copy;</span> {{ year }} Diky2020.cz
+      </div>
+      <a href="https://noltio.com/" class="noltio__link">
+        Vyvinulo
+        <img class="notlio__logo" src="/noltio-logo.png" alt="Logo firmy Noltio, s.r.o.">Noltio
+      </a>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 0;
+}
+
+.footer {
+  background-color: $yellow-light;
+  color: $black;
+  padding: 1.5rem 0;
+}
+
+.footer__wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  padding-right: 2.4rem;
+  font-size: 11px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+
+  @include sm {
+    flex-direction: column;
+
+    > * {
+      margin-bottom: 0.7rem;
+    }
+  }
+
+  &::after {
+    content: '';
+    background-image: url('/darce-pulka.svg');
+    height: 100px;
+    width: 70px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    position: absolute;
+    bottom: -4.25rem;
+    right: 0;
+    transform: rotateY(180deg);
+    z-index: 0;
+
+    @include sm {
+      bottom: -2.7rem;
+    }
+  }
+}
+
+.notlio__logo {
+  height: 0.9em;
+  margin-right: 2px;
+  margin-left: 2px;
+  top: 1px;
+  position: relative;
+}
+
+.noltio__link,
+.mail-link {
+  color: $black;
+  text-decoration: none;
+  transition: 0.2s;
+  z-index: 1;
+  border-bottom: 1px solid $black;
+
+  &:hover {
+    color: $orange;
+    border-bottom: 1px solid $orange;
+  }
+}
+
+.mail-link {
+  padding-left: 4px;
+}
+
+.icon--email {
+  background-image: url('/email.svg');
+  font-size: 0.8em;
+  top: 2px;
+  margin-right: 1px;
+}
+
+.copyright {
+  color: $orange;
+}
+
+.copyright__icon {
+  font-size: 12px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div id="web">
     <MainHeader />
     <Nuxt />
+    <MainFooter />
   </div>
 </template>
 
@@ -15,6 +16,7 @@
 
 #web {
   color: $black;
+  overflow: hidden;
 }
 
 .container {
@@ -33,5 +35,111 @@
   display: inline-block;
   background-size: contain;
   background-repeat: no-repeat;
+}
+
+.nuxt-content {
+  h2 {
+    font-weight: 800;
+    margin-top: 3.2rem;
+    font-size: 2.4rem;
+    margin-bottom: 1.2rem;
+
+    @include lg {
+      font-size: 1.7rem;
+      margin-top: 2rem;
+    }
+
+    a {
+      display: none;
+    }
+  }
+
+  h3 {
+    font-weight: 700;
+    margin-top: 3.2rem;
+    font-size: 1.8rem;
+    margin-bottom: 1.2rem;
+    @include lg {
+      font-size: 1.4rem;
+      margin-top: 2rem;
+      margin-bottom: 0.7rem;
+    }
+  }
+
+  p {
+    margin-bottom: 1.8rem;
+    font-size: 1.5rem;
+    line-height: 2.25rem;
+    margin-top: 0;
+    margin-bottom: 1.3rem;
+
+    @include lg {
+      font-size: 1.2rem;
+      line-height: 1.9rem;
+      margin-top: 0;
+      margin-bottom: 1.3rem;
+    }
+  }
+
+  a {
+    color: #684ada;
+
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  code {
+    font-family: monospace;
+    background: #f3f3f3;
+    color: #000;
+  }
+
+  pre {
+    border-left: 0;
+    overflow-x: scroll;
+    background: #222;
+    font-family: monospace;
+    font-size: 1.2rem;
+    line-height: 1.3rem;
+    border-radius: 8px;
+    padding: 1rem;
+    margin-bottom: 1.8rem;
+    @include lg {
+      margin-bottom: 1.3rem;
+    }
+
+    code {
+      background: #222;
+      color: #fff;
+    }
+  }
+
+  ul,
+  ol {
+    font-size: 1.5rem;
+    margin-left: 3rem;
+    margin-bottom: 2rem;
+    @include lg {
+      margin-left: 1.75rem;
+      margin-bottom: 1.5rem;
+    }
+
+    li {
+      margin-bottom: 0.5rem;
+      font-size: 1.5rem;
+      line-height: 2.25rem;
+      @include lg {
+        font-size: 1.2rem;
+        line-height: 1.9rem;
+      }
+    }
+  }
+
+  img {
+    max-width: 100%;
+    border: 1px solid #eee;
+    border-radius: 8px;
+  }
 }
 </style>
